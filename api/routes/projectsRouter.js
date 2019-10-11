5368320926df427c51b10726c2dd26ec370f3fc8
Moses Samuel <mosamuel98@gmail.com>
2019-10-11 09:50:35 -0700

    add boilerplate for the routers

---
+++ api/routes/projectsRouter.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const ProjectsModel = require('../models/ProjectsModel')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get('/:id', async (req, res) => {
+
+})
+
+router.get('/:id/tasks', async (req, res) => {
+
+})
+
+router.get('/:id/resources', async (req, res) => {
+    
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.put('/:id', async (req, res) => {
+
+})
+
+router.delete('/:id', async (req, res) => {
+
+})
---
+++ api/routes/resourcesRouter.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const ResourcesModel = require('../models/ResourcesModel')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+
+})
+
+router.get('/:id', async (req, res) => {
+
+})
+
+router.get('/projects', async (req, res) => {
+    
+})
+
+router.get('/:id/projects', async (req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.put('/:id', async (req, res) => {
+
+})
+
+router.delete('/:id', async (req, res) => {
+
+})
