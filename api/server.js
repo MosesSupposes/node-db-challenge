27792637353d56b10654541d578307603544f116
Moses Samuel <mosamuel98@gmail.com>
2019-10-11 09:58:06 -0700

    fix typo in endpoint for tasks

--- api/server.js
+++ api/server.js
@@ -8,6 +8,6 @@ const server = express()
 server.use(express.json())
 server.use('/api/projects', projectsRouter)
 server.use('/api/resources', resourcesRouter)
-server.use('/api//tasks', tasksRouter)
+server.use('/api/tasks', tasksRouter)
 
 module.exports = server
